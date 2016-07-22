541a4ee4d914e0ced174c6cecfe894d7dfc901a0
quanxi613 <quanxi613@126.com>
2016-07-22 16:05:00 +0800

    ReactDOM

--- app/main.js
+++ app/main.js
@@ -1,8 +1,3 @@
-/*'use strict';
-var component = require('./component.js');
-//import component from './component.js';
-
-document.body.appendChild(component());*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from './component.jsx';
