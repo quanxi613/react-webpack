3d4c9801c9b88e591ed5d79da4a348654d13ce26
quanxi613 <quanxi613@126.com>
2016-07-22 15:03:54 +0800

    修改

--- app/main.js
+++ app/main.js
@@ -1,4 +1,14 @@
-'use strict';
+/*'use strict';
 var component = require('./component.js');
+//import component from './component.js';
 
-document.body.appendChild(component());
+document.body.appendChild(component());*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './component.jsx';
+
+main();
+
+function main() {
+    ReactDOM.render(<Hello />, document.getElementById('app'));
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,25 @@
-var webpack = require('webpack');
-//var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
 var path = require('path');
 
 module.exports = {
     entry: {
-        helloworld: [
-            
-            path.resolve(__dirname, 'app/main.js')
-        ]
+    	helloworld: [
+	    	'webpack/hot/dev-server',
+	    	'webpack-dev-server/client?http://localhost:8080',
+	    	path.resolve(__dirname, 'app/main.js')
+    	],
     },
     output: {
-        path: path.resolve(__dirname,'./dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
     },
     module: {
-        loaders: [{
-            test: /\.jsx$/,
-            loader: 'babel-loader!jsx-loader?harmony'
-        }, {
-            test: /\.js?$/,
-            loaders: ['react-hot', 'babel'],
-            include: [path.join(__dirname, 'scripts')]
-        }, {
-            test: /\.less$/,
-            loader: 'style-loader!css-loader!autoprefixer-loader!less-loader'
-        }, { 
-            test: /\.(png|jpg)$/, 
-            loader: 'url-loader?limit=8192'
-        }]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
-    ]
+	    loaders: [{
+		    test: /\.jsx?$/, 
+		    exclude: /node_modules/, 
+		    loader: 'babel',
+		    query: {
+		        presets:['es2015','stage-0','react']
+		    }
+	    }]
+	}
 };
